1dd4e240a811d263dda1b91f5bebcbbb156893ff
benspector-mls <ben@marcylabschool.org>
2025-11-01 22:18:10 -0400

    adding bank solution

--- src/bank.js
+++ src/bank.js
@@ -5,43 +5,56 @@ class Bank {
   }
 
   addAccount(account) {
-    accounts.push(account);
+    // accounts.push(account);
+    this.accounts.push(account);
   }
 
   getTotalBalance() {
     let total = 0;
     for (let account of this.accounts) {
-      total += account.balance;
+      // total += account.balance;
+      total += account.getBalance();
     }
     return total;
   }
 
   findAccount(accountNumber) {
-    return accounts.find((account) => account.accountNumber = accountNumber);
+    // return accounts.find((account) => account.accountNumber = accountNumber);
+    return this.accounts.find((account) => account.accountNumber = accountNumber);
   }
 }
 
 class BankAccount {
+  #balance = 0;
+  static #totalAccounts = 0;
   constructor(accountNumber, ownerName) {
     this.accountNumber = accountNumber;
     this.ownerName = ownerName;
-    balance = 0;
-    totalAccounts++;
+    BankAccount.#totalAccounts++;
   }
 
   deposit(amount) {
-    this.balance += amount;
-    console.log(`Deposited $${amount}. New balance: $${balance}`);
+    // this.balance += amount;
+    this.#balance += amount;
+    // console.log(`Deposited $${amount}. New balance: $${this.balance}`);
+    console.log(`Deposited $${amount}. New balance: $${this.#balance}`);
   }
 
   withdraw(amount) {
-    this.balance -= amount;
-    console.log(`Withdrew $${amount}. New balance: $${this.balance}`);
+    // this.balance -= amount;
+    this.#balance -= amount;
+    // console.log(`Withdrew $${amount}. New balance: $${this.balance}`);
+    console.log(`Withdrew $${amount}. New balance: $${this.#balance}`);
 
   }
 
   getBalance() {
-    return balance;
+    // return balance;
+    return this.#balance;
+  }
+
+  static getTotalAccounts() {
+    return BankAccount.#totalAccounts;
   }
 }
 
@@ -58,3 +71,6 @@ account2.deposit(250);
 
 console.log("Total bank balance:", myBank.getTotalBalance()); // Should be 350
 console.log("Found account:", myBank.findAccount("001").ownerName); // Should be "Alice"
+
+const account3 = new BankAccount("003", "Charlie");
+console.log("Total accounts:", BankAccount.getTotalAccounts()); // Should be 3
